dc568d7558cc0cdd77cfb3cde88332c646d60b35
vinisklin <vinisklin@gmail.com>
2020-08-17 14:30:07 -0300

    Create: Forgot password page layout

--- src/pages/ForgotPassword/styles.ts
+++ src/pages/ForgotPassword/styles.ts
@@ -1,7 +1,7 @@
 import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
-import signUpBackgroundImg from '../../assets/sign-up-bg.png';
+import signInBackgroundImg from '../../assets/sign-in-background.png';
 
 export const Container = styled.div`
   height: 100vh;
@@ -20,10 +20,10 @@ export const Content = styled.div`
   max-width: 700px;
 `;
 
-const appearFromRight = keyframes`
+const appearFromLeft = keyframes`
   from {
     opacity: 0;
-    transform: translateX(50px);
+    transform: translateX(-50px);
   }
   to {
     opacity: 1;
@@ -37,7 +37,7 @@ export const AnimationContainer = styled.div`
   align-items: center;
   justify-content: center;
 
-  animation: ${appearFromRight} 1s;
+  animation: ${appearFromLeft} 1s;
 
   form {
     margin: 80px 0;
@@ -83,6 +83,6 @@ export const AnimationContainer = styled.div`
 
 export const Background = styled.div`
   flex: 1;
-  background: url(${signUpBackgroundImg}) no-repeat center;
+  background: url(${signInBackgroundImg}) no-repeat center;
   background-size: cover;
 `;
